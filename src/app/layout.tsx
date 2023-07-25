1b3cfccd657c3ca8a629dfabedbbd1b6b6027073
pedromaranini <pedrolucasmaranini30@gmail.com>
2023-07-25 18:38:59 -0300

    upload files / middleware routes / logout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
-import './globals.css'
 import type { Metadata } from 'next'
+import { Hero } from '@/components/Hero'
+import { Profile } from '@/components/Profile'
+import { Copyright } from '@/components/Copyright'
+import { SignIn } from '@/components/SignIn'
+import { cookies } from 'next/headers'
 import { Roboto_Flex as Roboto, Bai_Jamjuree as BaiJamjuree } from 'next/font/google'
+import './globals.css'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 const baiJamjuree = BaiJamjuree({ subsets: ['latin'], weight: '700', variable: '--font-bai-jamjuree' })
@@ -10,14 +15,31 @@ export const metadata: Metadata = {
   description: 'Uma cápsula do tempo construída com React, Next.js, TailwindCSS e TypeScript',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <html lang="en">
-      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans text-gray-100 bg-gray-900`}>{children}</body>
+      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans text-gray-100 bg-gray-900`}>
+        <main className="grid grid-cols-2 min-h-screen">
+          {/* Section Left */}
+          <div className="flex flex-col items-start justify-between px-28 py-16 bg-[url(../assets/bg-stars.svg)] bg-cover relative overflow-hidden border-r border-white/10">
+            {/* Blur */}
+            <div className="absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/2 h-[288px] w-[526px] rounded-full bg-purple-700 opacity-50 blur-full" />
+
+            {/* Stripes */}
+            <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
+            {isAuthenticated ? <Profile /> : <SignIn />}
+            <Hero />
+            <Copyright />
+          </div>
+
+          {/* Section Right */}
+          <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,7 @@
-import { User } from 'lucide-react'
-import Image from 'next/image'
-
-import nlwLogo from '../assets/logo-nlw.svg';
+import { EmptyMemories } from '@/components/EmptyMemories';
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-2 min-h-screen">
-      {/* Section Left */}
-      <div className="flex flex-col items-start justify-between px-28 py-16 bg-[url(../assets/bg-stars.svg)] bg-cover relative overflow-hidden border-r border-white/10">
-        {/* Blur */}
-        <div className="absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/2 h-[288px] w-[526px] rounded-full bg-purple-700 opacity-50 blur-full" />
-
-        {/* Stripes */}
-        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
-
-        {/* Profile/Sign In */}
-        <a href="" className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
-
-          <p className="max-w-[8.75rem] text-sm leading-snug"><span className="underline">Crie sua conta</span> e salve suas memórias!</p>
-        </a>
-
-        {/* Section Hero */}
-        <div className="space-y-5">
-          <Image src={nlwLogo} alt="Logo NLW Spacetime" />
-
-          <div className="max-2-[26.25rem] space-y-1">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">Sua cápsula do tempo</h1>
-            <p className="text-lg leading-relaxed">Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</p>
-          </div>
-          <a
-            href=""
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
-          >
-            CADASTRAR LEMBRANÇA
-          </a>
-        </div>
-
-        {/* Copyright */}
-        <div className="text-sm leading-relaxed text-gray-200">
-          Feito com 💜 no NLW da{' '}
-          <a target='_blank' rel="noreferrer" href="https://rocketseat.com.br" className="underline hover:text-gray-100">Rocketseat</a>
-        </div>
-      </div>
-
-      {/* Section Right */}
-      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="text-center leading-relaxed w-[22.5rem]">
-            Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <a href="" className="underline hover:text-gray-50">criar agora</a>!
-          </p>
-        </div>
-      </div>
-    </main>
+    <EmptyMemories />
   )
 }
